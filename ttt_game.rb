7b770d5cfe7091e7333e21eebbe578663939850e
nanarama <annasampers@gmail.com>
2022-06-10 17:42:21 -0500

    Add GamePlay class and take_turn methods, doesn't quite work yet

--- ttt_game.rb
+++ ttt_game.rb
@@ -1,22 +1,29 @@
-#board = [
-#  [o,o,o]
-#  [x,o,x]
-#  [o,x,x]
-#  ]
-
 class GameSetUp 
   @@games = 0
   @@num_of_players = 0
   @@someone_is_first = false
-  @@pieces = ['x', 'o']
+  @@pieces = ['X', 'O']
   
+  attr_accessor :board
+
   def self.start_game
     puts "Let's play Tic Tac Toe!"
     @@games +=1
   end
+
+  def initialize
+    @board = [
+    ["[]","[]","[]"].join,
+    ["[]","[]","[]"].join,
+    ["[]","[]","[]"].join
+  ]
+  end
 end 
 
 class Player < GameSetUp
+  @@player1_turns = 0
+  @@player2_turns = 0
+
   attr_accessor :name, :num_of_turns, :piece
   
   def initialize
@@ -46,38 +53,85 @@ class Player < GameSetUp
       answer = gets.chomp.downcase
         if answer == "y" && @@num_of_players == 1
           puts "Great."
-          @num_of_turns = 5
+          @@player1_turns = 5
+          @num_of_turns = @@player1_turns
           @@someone_is_first = true
         elsif answer == 'n' && @@num_of_players == 1
           puts "That's okay. You don't have to."
-          @num_of_turns = 4
+          @@player1_turns = 4
+          @num_of_turns = @@player1_turns
         else answer == 'n' && @@num_of_players == 2
           puts "Well, someone has to. so it's you!"
-          @num_of_turns = 5
+          @@player2_turns = 5
+          @num_of_turns = @@player2_turns
         end
     else @@someone_is_first == true
-      puts "Your opponent already chose to go first, so you'll go second."
-      @num_of_turns = 4
+      puts "You'll go second."
+      @@player2_turns = 4
+      @num_of_turns = @@player2_turns
     end 
   end
 
   def piece_selection
     if @@num_of_players == 1
       puts "Would you like to be the 'X' or the 'O'?"
-      answer = gets.chomp.downcase
-      if answer == 'x'
+      answer = gets.chomp.upcase
+      if answer == 'X'
         @piece = @@pieces.shift
       else 
         @piece = @@pieces.pop
       end
     else 
-      puts "Your opponent already chose their piece so you have to be the #{@@pieces[0].upcase}."
+      puts "You'll be the #{@@pieces[0]}."
       @piece = @@pieces.pop
     end  
   end
 
   def info
-    "My name is #{@name}, I have #{@num_of_turns} turns left, and my piece is the #{@piece.upcase}."
+    "My name is #{@name}, I have #{@num_of_turns} turns left, and my piece is the #{@piece}."
+  end
+
+  def player1_take_turn
+    puts "#{player1.name}, where would you like to put your #{player1.piece}?"
+    puts "Row: "
+    row = gets.chomp.to_i
+    puts "Column: "
+    column = gets.chomp.to_i
+    board.board[row-1][column-1] = "#{player1.piece} "
+    player1.num_of_turns -= 1
+  end
+
+  def player2_take_turn
+    puts "#{player2.name}, where would you like to put your #{player2.piece}?" 
+    puts "Row: 1, 2, or 3?"
+    row = gets.chomp.to_i
+    puts "Column: 1, 2, or 3?"
+    column = gets.chomp.to_i
+    board.board[row-1][column-1] = "#{player2.piece} "
+    player2.num_of_turns -= 1
+  end
+end
+
+class GamePlay < Player
+
+  @@win = ''
+  def initialize
+  end
+
+  def play_a_round
+    if @@player1_turns != 0
+      player1_take_turn
+    end
+    if @@player2_turns != 0
+      player2_take_turn
+    end
+  end
+  
+
+  def check_for_win
+    if board.board[0..2][0..2] == player1.piece
+      @@win = "Congratulations, #{player1.name}! You won!"
+    end  
   end
   
 end
@@ -86,3 +140,9 @@ player1 = Player.new
 player2 = Player.new
 puts player1.info
 puts player2.info
+board = GameSetUp.new
+puts "Here is the board:\n"
+puts board.board
+game = GamePlay.new
+game.play_a_round
+puts board.board
