bb964d08c8ebd286180874350fe91d4b045845f6
nanarama <annasampers@gmail.com>
2022-06-14 12:07:36 -0500

    Fix tie game option, clean up unused variables.

--- ttt_game.rb
+++ ttt_game.rb
@@ -1,6 +1,5 @@
 class GameSetUp 
   
-  @@games = 0
   @@num_of_players = 0
   @@someone_is_first = false
   @@total_turns = 9
@@ -17,72 +16,74 @@ class GameSetUp
   def initialize
   end
 
-  def self.start_game
-    puts "Let's play Tic Tac Toe!"
-    @@games +=1
-  end
-
   def self.puts_board
     puts @@board[0][0..2].join
     puts @@board[1][0..2].join
     puts @@board[2][0..2].join
   end
 
-  def self.puts_board_flatten
-    puts @@board.flatten[0..2]
-  end
-
   def self.check_for_win
     @@checking = @@board.flatten
-    if @@total_turns == 0
-      puts "Game over. It's a tie."
-    elsif @@checking[0..2].join == "XXX" || @@checking[3..5].join == "XXX" || @@checking[6..8].join == "XXX"
+    if @@checking[0..2].join == "XXX" || @@checking[3..5].join == "XXX" || @@checking[6..8].join == "XXX"
       puts "X wins!"
       @@total_turns = 0
+      @@winner = 'true'
       return
     elsif @@checking[0] == "X" && @@checking[3] == "X" && @@checking[6] == "X"
       puts "X wins!"
       @@total_turns = 0
+      @@winner = 'true'
       return
     elsif @@checking[1] == "X" && @@checking[4] == "X" && @@checking[7] == "X" 
       puts "X wins!" 
       @@total_turns = 0
+      @@winner = 'true'
       return
     elsif @@checking[2] == "X" && @@checking[5] == "X" && @@checking[8] == "X"   
       puts "X wins" 
       @@total_turns = 0
+      @@winner = 'true'
       return
     elsif @@checking[0] == "X" && @@checking[4] == "X" && @@checking[8] == "X"   
       puts "X wins"
       @@total_turns = 0
+      @@winner = 'true'
+      return
     elsif @@checking[6] == "X" && @@checking[4] == "X" && @@checking[2] == "X"
       puts "X wins"
       @@total_turns = 0
+      @@winner = 'true'
       return
     elsif @@checking[0..2].join == "OOO" || @@checking[3..5].join == "OOO" || @@checking[6..8].join == "OOO"
       puts "O wins!"
       @@total_turns = 0
+      @@winner = 'true'
       return
     elsif @@checking[0] == "O" && @@checking[3] == "O" && @@checking[6] == "O"
       puts "O wins!"
       @@total_turns = 0
+      @@winner = 'true'
       return
     elsif @@checking[1] == "O" && @@checking[4] == "O" && @@checking[7] == "O" 
       puts "O wins!"  
       @@total_turns = 0
+      @@winner = 'true'
       return
     elsif @@checking[2] == "O" && @@checking[5] == "O" && @@checking[8] == "O"   
       puts "O wins" 
       @@total_turns = 0
+      @@winner = 'true'
       return
     elsif @@checking[0] == "O" && @@checking[4] == "O" && @@checking[8] == "O"   
       puts "O wins"
       @@total_turns = 0
+      @@winner = 'true'
       return
     elsif @@checking[6] == "O" && @@checking[4] == "O" && @@checking[2] == "O"
       puts "O wins"  
-      @@total_turns = 0 
-      return 
+      @@total_turns = 0
+      @@winner = 'true' 
+      return  
     end  
   end
 end 
@@ -101,7 +102,7 @@ class GamePlay < GameSetUp
 
   def initialize
     if @@num_of_players == 0
-      GameSetUp.start_game
+      puts "Let's play Tic Tac Toe!"
     end 
     get_player_name
     piece_selection
@@ -168,7 +169,10 @@ class GamePlay < GameSetUp
         player2_turn
         @@total_turns -= 1
       end   
-    end          
+    end 
+    if @@total_turns == 0 && @@winner == 'false'   
+      puts "Game over. It's a tie."
+    end      
   end
 
 end
