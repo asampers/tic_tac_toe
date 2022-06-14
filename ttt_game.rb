290bcd8fd93374116dc65bd150b1aae437da8df2
nanarama <annasampers@gmail.com>
2022-06-13 20:39:07 -0500

    Change player1 and 2 to class variables, rework player_turn methods and play_round. Check_win doesn't work yet.

--- ttt_game.rb
+++ ttt_game.rb
@@ -1,10 +1,13 @@
 require 'pry-byebug'
 
 class GameSetUp 
+  
   @@games = 0
   @@num_of_players = 0
   @@someone_is_first = false
+  @@total_turns = 9
   @@pieces = ['X', 'O']
+  @@winner = 'false'
   @@board = [
     ["_.","_.","_."],
     ["_.","_.","_."],
@@ -12,6 +15,9 @@ class GameSetUp
   ]
 
   attr_accessor :board
+
+  def initialize
+  end
 
   def self.start_game
     puts "Let's play Tic Tac Toe!"
@@ -24,57 +30,93 @@ class GameSetUp
     puts @@board[2][0..2].join
   end
 
-  def initialize
+  def self.check_for_win
+    @@checking = @@board.flatten
+    if @@checking[0..2] == "XXX" || @@checking[3..5] == "XXX" || @@checking[6..8] == "XXX"
+      puts "X wins!"
+      @@total_turns = 0
+      @@winner = 'true'
+    elsif @@checking[0] == "X" && @@checking[3] == "X" && @@checking[6] == "X"
+      puts "X wins!"
+      @@total_turns = 0
+      @@winner = 'true'
+    elsif @@checking[1] == "X" && @@checking[4] == "X" && @@checking[7] == "X" 
+      puts "X wins!" 
+      @@total_turns = 0
+      @@winner = 'true'
+    elsif @@checking[2] == "X" && @@checking[5] == "X" && @@checking[8] == "X"   
+      puts "X wins" 
+      @@total_turns = 0
+      @@winner = 'true'
+    elsif @@checking[0] == "X" && @@checking[4] == "X" && @@checking[8] == "X"   
+      puts "X wins"
+      @@total_turns = 0
+      @@winner = 'true'
+    elsif @@checking[6] == "X" && @@checking[4] == "X" && @@checking[2] == "X"
+      puts "X wins"
+      @@total_turns = 0
+      @@winner = 'true'
+    elsif @@checking[0..2] == "OOO" || @@checking[3..5] == "OOO" || @@checking[6..8] == "OOO"
+      puts "O wins!"
+      @@total_turns = 0
+      @@winner = 'true'
+    elsif @@checking[0] == "O" && @@checking[3] == "O" && @@checking[6] == "O"
+      puts "O wins!"
+      @@total_turns = 0
+      @@winner = 'true'
+    elsif @@checking[1] == "O" && @@checking[4] == "O" && @@checking[7] == "O" 
+      puts "O wins!"  
+      @@total_turns = 0
+      @@winner = 'true'
+    elsif @@checking[2] == "O" && @@checking[5] == "O" && @@checking[8] == "O"   
+      puts "O wins" 
+      @@total_turns = 0
+      @@winner = 'true'
+    elsif @@checking[0] == "O" && @@checking[4] == "O" && @@checking[8] == "O"   
+      puts "O wins"
+      @@total_turns = 0
+      @@winner = 'true'
+    elsif @@checking[6] == "O" && @@checking[4] == "O" && @@checking[2] == "O"
+      puts "O wins"  
+      @@total_turns = 0 
+      @@winner = 'true'
+    elsif @@total_turns == 0
+       puts "Game over. It's a tie." 
+       @@winner = 'true' 
+    end  
   end
 end 
 
 class GamePlay < GameSetUp
-  attr_accessor :name, :num_of_turns, :piece
   
+  @@player1 = {
+    name: "",
+    piece: ""
+  }
+
+  @@player2 = {
+    name: "",
+    piece: ""
+  }
+
   def initialize
     if @@num_of_players == 0
       GameSetUp.start_game
     end 
     get_player_name
-    player_order
     piece_selection
   end
 
   def get_player_name
     if @@num_of_players == 0
       puts "Please enter the name of Player 1"
-      @name = gets.chomp
+      @@player1[:name] = gets.chomp
       @@num_of_players +=1
     else 
       puts "Now enter the name of Player 2"
-      @name = gets.chomp
+      @@player2[:name] = gets.chomp
       @@num_of_players +=1
     end
-  end
-
-  def player_order
-    if @@someone_is_first == false
-      puts "Would you like to go first? Y / N"
-      answer = gets.chomp.downcase
-        if answer == "y" && @@num_of_players == 1
-          puts "Great."
-          @@player1_turns = 5
-          @num_of_turns = @@player1_turns
-          @@someone_is_first = true
-        elsif answer == 'n' && @@num_of_players == 1
-          puts "That's okay. You don't have to."
-          @@player1_turns = 4
-          @num_of_turns = @@player1_turns
-        else answer == 'n' && @@num_of_players == 2
-          puts "Well, someone has to. so it's you!"
-          @@player2_turns = 5
-          @num_of_turns = @@player2_turns
-        end
-    else @@someone_is_first == true
-      puts "You'll go second."
-      @@player2_turns = 4
-      @num_of_turns = @@player2_turns
-    end 
   end
 
   def piece_selection
@@ -82,85 +124,60 @@ class GamePlay < GameSetUp
       puts "Would you like to be the 'X' or the 'O'?"
       answer = gets.chomp.upcase
       if answer == 'X'
-        @piece = @@pieces.shift
+        @@player1[:piece] = @@pieces.shift
       else 
-        @piece = @@pieces.pop
+        @@player1[:piece] = @@pieces.pop
       end
     else 
-      puts "You'll be the #{@@pieces[0]}."
-      @piece = @@pieces.pop
+      puts "#{@@player2[:name]} will be the #{@@pieces[0]}."
+      @@player2[:piece] = @@pieces.pop
     end  
   end
 
-  def info
-    "My name is #{@name}, I have #{@num_of_turns} turns left, and my piece is the #{@piece}."
-  end
-
-  def take_turn
-    puts "#{@name}, where would you like to put your #{@piece}?"
+  def self.player1_turn
+    puts "#{@@player1[:name]}, where would you like to put your #{@@player1[:piece]}?"
     puts "Row: 1, 2, or 3?"
     row = gets.chomp.to_i - 1 
     puts "Column: 1, 2, or 3?"
     column = gets.chomp.to_i - 1
     @@board[row][column] = ""
-    @@board[row][column] = "#{@piece}"
-    @num_of_turns -= 1
+    @@board[row][column] = "#{@@player1[:piece]}"
+    puts "________"
+    GameSetUp.puts_board
   end
-end
 
-class GameWin < GameSetUp
-
-  @@win = ''
-  def initialize
-  end
-  
-
-  def self.check_for_win
-    @@checking = @@board.flatten
-    if @@checking[0..2] == "XXX" || @@checking[3..5] == "XXX" || @@checking[6..8] == "XXX"
-      puts "X wins!"
-    elsif @@checking[0] == "X" && @@checking[3] == "X" && @@checking[6] == "X"
-      puts "X wins!"
-    elsif @@checking[1] == "X" && @@checking[4] == "X" && @@checking[7] == "X" 
-      puts "X wins!"  
-    elsif @@checking[2] == "X" && @@checking[5] == "X" && @@checking[8] == "X"   
-      puts "X wins" 
-    elsif @@checking[0] == "X" && @@checking[4] == "X" && @@checking[8] == "X"   
-      puts "X wins"
-    elsif @@checking[6] == "X" && @@checking[4] == "X" && @@checking[2] == "X"
-      puts "X wins"
-    elsif @@checking[0..2] == "OOO" || @@checking[3..5] == "OOO" || @@checking[6..8] == "OOO"
-      puts "O wins!"
-    elsif @@checking[0] == "O" && @@checking[3] == "O" && @@checking[6] == "O"
-      puts "O wins!"
-    elsif @@checking[1] == "O" && @@checking[4] == "O" && @@checking[7] == "O" 
-      puts "O wins!"  
-    elsif @@checking[2] == "O" && @@checking[5] == "O" && @@checking[8] == "O"   
-      puts "O wins" 
-    elsif @@checking[0] == "O" && @@checking[4] == "O" && @@checking[8] == "O"   
-      puts "O wins"
-    elsif @@checking[6] == "O" && @@checking[4] == "O" && @@checking[2] == "O"
-      puts "O wins"  
-    end  
+  def self.player2_turn
+    puts "#{@@player2[:name]}, where would you like to put your #{@@player2[:piece]}?"
+    puts "Row: 1, 2, or 3?"
+    row = gets.chomp.to_i - 1 
+    puts "Column: 1, 2, or 3?"
+    column = gets.chomp.to_i - 1
+    @@board[row][column] = ""
+    @@board[row][column] = "#{@@player2[:piece]}"
+    puts "________"
+    GameSetUp.puts_board
   end
   
+  def self.play_round
+    while @@total_turns > 0
+      if @@winner == 'false'
+        player1_turn
+        @@total_turns -= 1
+        GameSetUp.check_for_win
+          if @@winner == 'false'
+            player2_turn
+            @@total_turns -= 1
+            GameSetUp.check_for_win
+          end 
+      end     
+    end       
+  end
+
 end
+
 
 player1 = GamePlay.new
 player2 = GamePlay.new
-puts player1.info
-puts player2.info
 puts "Here is the board:\n"
 GameSetUp.puts_board
-player1.take_turn
-GameSetUp.puts_board
-player2.take_turn
-GameSetUp.puts_board
-player1.take_turn
-GameSetUp.puts_board
-player2.take_turn
-GameSetUp.puts_board
-player1.take_turn
-GameSetUp.puts_board
-binding.pry
-GameWin.check_for_win
+GamePlay.play_round
